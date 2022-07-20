235ed2e0d80fbcf7f2eeca0bb82ea7935cfb2aed
Mohsen <42637402+MohsenFaghih@users.noreply.github.com>
2022-07-21 00:09:04 +0430

    configure webpack and server file to reduce its size

--- src/index.js
+++ src/index.js
@@ -1,11 +1,5 @@
-// const express = require('express');
-// const React = require('react');
-// const renderToString = require('react-dom/server').renderToString;
-// const Home = require('./client/components/Home.js').default;
 import express from 'express';
-import React from 'react';
-import {renderToString} from 'react-dom/server';
-import Home from './client/components/Home.js';
+import Renderer from './helpers/Renderer';
 
 const app = express();
 const PORT = 3000;
@@ -13,19 +7,7 @@ const PORT = 3000;
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-    const content = renderToString(<Home />);
-
-    const html = `
-        <html>
-            <head></head>
-            <body>
-                <div id="root">${content}</div>
-                <script src="bundle.js"></script>
-            </body>
-        </html>
-    `
-
-    res.send(html);
+    res.send(Renderer());
 })
 
 app.listen(PORT, () => {
--- webpack.server.js
+++ webpack.server.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const merge = require('webpack-merge');
 const baseConfig = require('./webpack.base.js');
+const webpackNodeExternals = require('webpack-node-externals');
 
 const config = {
     // import webpack that we are building a bundle for nodejs, rather than for the browser
@@ -13,7 +14,10 @@ const config = {
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'build')
-    }
+    },
+
+    // It make bundle lighter because of avoiding adding all node_module to it
+    externals: [webpackNodeExternals()]
 }
 
 module.exports = merge(baseConfig, config)
